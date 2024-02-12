54a1b27605942fc73e246408aa8d7f63e6d26368
goolsbyash <goolsbyashley015@gmail.com>
2024-02-12 11:50:50 -0500

    Added basic form for updating account info.

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,7 +1,32 @@
-export default function Settings() {
-    return (
-        <div>
-            <h2>Account Settings</h2>
-        </div>
-    )
+export default function Settings({ user }) {
+  const { _id } = user;
+  const handleUpdate = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2>Account Settings</h2>
+      <form onSubmit={handleUpdate}>
+        <input type="text" name="firstName" placeholder="First Name:" />
+        <input type="text" name="lastName" placeholder="Last Name:" />
+        <input type="email" name="email" placeholder="Update email:" />
+        <input
+          type="password"
+          name="oldPassword"
+          placeholder="Current Password:"
+        />
+        <input
+          type="password"
+          name="newPassword"
+          placeholder="Enter New Password:"
+        />
+        <input
+          type="password"
+          name="newPassword2"
+          placeholder="Confirm New Password:"
+        />
+      </form>
+    </div>
+  );
 }
