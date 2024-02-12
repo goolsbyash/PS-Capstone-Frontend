dc140b29d5533910bb05319f09d07ba3dc86c24f
goolsbyash <goolsbyashley015@gmail.com>
2024-02-12 10:02:57 -0500

    Started building other pages.

---
+++ src/pages/BuildPlan.js
@@ -0,0 +1,7 @@
+export default function BuildPlan() {
+    return (
+        <div>
+            <h2>Build Custom Workout Plan</h2>
+        </div>
+    )
+}
---
+++ src/pages/Settings.js
@@ -0,0 +1,7 @@
+export default function Settings() {
+    return (
+        <div>
+            <h2>Account Settings</h2>
+        </div>
+    )
+}
