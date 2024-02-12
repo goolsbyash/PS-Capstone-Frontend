e35f6dcc15013fe6e9e05f344ac114d08da94e42
goolsbyash <goolsbyashley015@gmail.com>
2024-02-11 22:18:58 -0500

    Added Route components and useContext hook.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { useEffect, useState } from "react";
+import NavBar from "./components/NavBar";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import { UserContext } from "./context/UserContext";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [exercies, setExercies] = useState([]);
+
+  useEffect(() => {
+    // fetch data from API
+    const fetchData = async () => {
+      const res = await fetch("https://wger.de/api/v2/exercise/?language=2");
+      const data = await res.json();
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={{ user, setUser }}>
+      {/* <form onSubmit={handleSubmit}> */}
+      <h1>BodyMorph App</h1>
+      {user ? (
+        <>
+          <NavBar/>
+          <Routes>
+            <Route path={`/dashboard/user/${user}`} element={<Dashboard />} />
+            <Route path="/dashboard/plans" />
+          </Routes>
+        </>
+      ) : (
+        <MainPage />
+      )}
+    </UserContext.Provider>
   );
 }
 
