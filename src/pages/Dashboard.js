07499fdac0f999b270f54dfe8c8a9e953ee4b311
goolsbyash <goolsbyashley015@gmail.com>
2024-02-13 13:40:42 -0500

    Added get request to fetch saved plans by owner.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,36 @@
-export default function Dashboard() {
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+export default function Dashboard({ user }) {
+  const { activePlan, _id } = user;
+  const fetchPlans = async () => {
+    try {
+      const res = await axios.get(`http://localhost:4000/api/plans/owner/${_id}`);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
   return (
     <div>
       <h2>Dashboard</h2>
+      <h3>Active Plan</h3>
+      {/* if no active plan, show button to link to /buildplan */}
+      {activePlan ? (
+        <h4>{activePlan}</h4>
+      ) : (<Link to={`/${_id}/buildplan`}>
+      <button>
+          No Active Plan? Build One
+        </button>
+      </Link>
+        
+      )}
+      <h3>Saved Plans</h3>
+      {/* if no saved plan, show button to link to /buildplan */}
+      {/* {fetchPlans} */}
+      {/* if number of saved plans is less than 4, include add more buttton */}
+      {/* make GET request db by owner id */}
     </div>
   );
 }
