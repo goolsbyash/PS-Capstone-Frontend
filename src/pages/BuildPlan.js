4410ea948daca89c3ee67334f4b3970bf9f1e700
goolsbyash <goolsbyashley015@gmail.com>
2024-02-13 13:36:57 -0500

    Added PlanSidebar component to render plan information.

--- src/pages/BuildPlan.js
+++ src/pages/BuildPlan.js
@@ -1,7 +1,14 @@
-export default function BuildPlan() {
+import ExerciseList from "../components/ExerciseList";
+import PlanSidebar from "../components/PlanSidebar";
+
+export default function BuildPlan({exercises}) {
+
     return (
         <div>
             <h2>Build Custom Workout Plan</h2>
+            <ExerciseList exercises={exercises}/>
+            {/* section to show added exercises plus buttons to change reps/sets */}
+            <PlanSidebar/>
         </div>
     )
 }
