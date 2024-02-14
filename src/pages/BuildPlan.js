79eea58b1922c0cd348f21ca524aa5f2f233d241
goolsbyash <goolsbyashley015@gmail.com>
2024-02-14 12:15:30 -0500

    Removed unneeded useContext.

--- src/pages/BuildPlan.js
+++ src/pages/BuildPlan.js
@@ -1,14 +1,14 @@
+// import { useContext } from "react";
 import ExerciseList from "../components/ExerciseList";
 import PlanSidebar from "../components/PlanSidebar";
 
-export default function BuildPlan({exercises}) {
-
-    return (
-        <div>
-            <h2>Build Custom Workout Plan</h2>
-            <ExerciseList exercises={exercises}/>
-            {/* section to show added exercises plus buttons to change reps/sets */}
-            <PlanSidebar/>
-        </div>
-    )
+export default function BuildPlan({ exercises }) {
+  return (
+    <div>
+      <h2>Build Custom Workout Plan</h2>
+      <ExerciseList exercises={exercises} />
+      // section to show added exercises
+      <PlanSidebar />
+    </div>
+  );
 }
