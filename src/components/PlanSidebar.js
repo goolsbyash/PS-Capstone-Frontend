17a8b83343512be951dd81b97c9cd731c9f49e3b
goolsbyash <goolsbyashley015@gmail.com>
2024-02-14 12:16:23 -0500

    Added function to set plan as active and post to db.

--- src/components/PlanSidebar.js
+++ src/components/PlanSidebar.js
@@ -1,17 +1,37 @@
 import SidePlanList from "./SidePlanList";
-// pass in UserContext
+import { useContext, useRef, useState } from "react";
+import { NewPlanContext } from "../context/NewPlanContext";
+import { UserContext } from "../context/UserContext";
+import axios from "axios";
 
-export default function PlanSidebar({ newPlan }) {
-  // TODO: Read local storage to view added exercises
+export default function PlanSidebar() {
+  const planCtx = useContext(NewPlanContext);
+  const { newPlan } = planCtx;
+  const userCtx = useContext(UserContext);
+  const { user } = userCtx;
+  const customNameRef = useRef(null);
+  const addPlan = {
+    exercises: newPlan,
+    owner: user._id,
+    active: false,
+  };
+  const handleActivePlan = (e) => {
+    if (addPlan.active) {
+      addPlan.active = false;
+    } else addPlan.active = true;
+    console.log(addPlan);
+  };
   const handleSavePlan = (e) => {
     e.preventDefault();
     // add exercise plan to db with user's _id
-    const addPlan = {
-      name: e.target.name,
-      exercises: newPlan,
-      owner: "",
-      active: true,
-    };
+    addPlan.name = customNameRef.current.value;
+    console.log(addPlan);
+    try {
+        const res = axios.post("http://localhost:4000/api/exercises", addPlan);
+        console.log(res.data);
+    } catch (error) {
+        console.log(error);
+    }
   };
 
   return (
@@ -20,10 +40,21 @@ export default function PlanSidebar({ newPlan }) {
       <h3>New Workout Plan</h3>
       <SidePlanList />
       <form onSubmit={handleSavePlan}>
-        <input type="text" name="name" placeholder="Custom Plan Name:" />
+        <input
+          type="text"
+          name="customName"
+          placeholder="Custom Plan Name:"
+          ref={customNameRef}
+        />
         <br />
         <label>Set as Active</label>
-        <input type="checkbox" name="activePlan" id="activePlan" />
+        <input
+          type="checkbox"
+          name="active"
+          value="active"
+          id="activePlan"
+          onClick={handleActivePlan}
+        />
         <br />
         <button type="submit">Save Plan</button>
       </form>
