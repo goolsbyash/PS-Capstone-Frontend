2b9d9243102f74072889e149bf2764123b02d4ad
goolsbyash <goolsbyashley015@gmail.com>
2024-02-14 09:45:04 -0500

    Added get request to fetch saved plans.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,17 +1,26 @@
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Dashboard({ user }) {
   const { activePlan, _id } = user;
-  const fetchPlans = async () => {
-    try {
-      const res = await axios.get(`http://localhost:4000/api/plans/owner/${_id}`);
-      console.log(res.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
+  const [viewPlan, setViewPlan] = useState(null);
+  useEffect(() => {
+    const fetchPlans = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:4000/api/plans/owner/${_id}`
+        );
+        // const data = await res.json();
+        console.log(res.data);
+        setViewPlan(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPlans();
+  }, []);
+
   return (
     <div>
       <h2>Dashboard</h2>
@@ -19,16 +28,22 @@ export default function Dashboard({ user }) {
       {/* if no active plan, show button to link to /buildplan */}
       {activePlan ? (
         <h4>{activePlan}</h4>
-      ) : (<Link to={`/${_id}/buildplan`}>
-      <button>
-          No Active Plan? Build One
-        </button>
-      </Link>
-        
+      ) : (
+        <Link to={`/${_id}/buildplan`}>
+          <button>No Active Plan? Build One</button>
+        </Link>
       )}
       <h3>Saved Plans</h3>
       {/* if no saved plan, show button to link to /buildplan */}
-      {/* {fetchPlans} */}
+      {viewPlan ? (
+        <Link to={`/${_id}/buildplan`}>
+          {viewPlan.length < 4 && <button>Add New Plan</button>}
+        </Link>
+      ) : (
+        <Link to={`/${_id}/buildplan`}>
+          <button>No Saved Plan? Build One</button>
+        </Link>
+      )}
       {/* if number of saved plans is less than 4, include add more buttton */}
       {/* make GET request db by owner id */}
     </div>
